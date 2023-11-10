f3b9107149d530f26db99086d72f4bae5ff0235e
larshubacher <lars.hubacher@bluewin.ch>
2023-11-10 18:10:55 +0100

    Slot is added and if a button is clicked or not is signaled

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ from button_holder import ButtonHolder
 #The sys module is responsible for processing commmand line arguments
 import sys
 
-def button_clicked():
-    print("You clicked the button, didn't you!")
+## The slot: respons when something happens
+def button_clicked(data):
+    print("You clicked the button, didn't you: ", data)
 
 
 app = QApplication(sys.argv)
 button = QPushButton("Press Me")
+button.setCheckable(True) ## Makes the button checkable. It's unchecked by default.
+## Future clicks toggle between checked and unchecked states
 
+## Clicked is a signal of QPushButton. It's emitted when you click onthe button
+## You can wire a slot to the signal using the syntax below:
 button.clicked.connect(button_clicked)
 
 button.show()
