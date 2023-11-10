3033e4a845f7878dc6f06689a69b35e752e8cb11
larshubacher <lars.hubacher@bluewin.ch>
2023-11-10 18:46:15 +0100

    Rockwidgets

--- main.py
+++ main.py
@@ -1,39 +1,10 @@
-#Importing the components we need
-from PySide6.QtWidgets import QApplication, QPushButton, QSlider
-from PySide6.QtCore import Qt
-from button_holder import ButtonHolder
-
-#The sys module is responsible for processing commmand line arguments
+from PySide6.QtWidgets import QApplication
+from rock_widget import RockWidget
 import sys
 
-## The slot: respons when something happens
-def respond_to_slider(data):
-    print("Slider moved to: ", data)
+app = QApplication(sys.argv)
 
+widget = RockWidget()
+widget.show()
 
-app = QApplication()
-
-slider = QSlider(Qt.Horizontal)
-slider.setMinimum(1)
-slider.setMaximum(100)
-slider.setValue(25)
-
-
-#button = QPushButton("Press Me")
-#button.setCheckable(True) ## Makes the button checkable. It's unchecked by default.
-## Future clicks toggle between checked and unchecked states
-
-## Clicked is a signal of QPushButton. It's emitted when you click onthe button
-## You can wire a slot to the signal using the syntax below:
-#button.clicked.connect(button_clicked)
-#button.show()
-
-#window = ButtonHolder()
-#window.show()
-
-## Do the connection. QT takes care of the passing of the data from the signal to the slot
-slider.valueChanged.connect(respond_to_slider)
-slider.show()
-
-#Start the event loop
 app.exec()
