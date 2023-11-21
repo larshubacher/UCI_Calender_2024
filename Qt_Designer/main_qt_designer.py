6d80ff496d4dd09a13b96de2d9794d286ae25f78
larshubacher <lars.hubacher@bluewin.ch>
2023-11-21 13:54:42 +0100

    User Interface with widget with Object

---
+++ Qt_Designer/main_qt_designer.py
@@ -0,0 +1,10 @@
+import sys
+from PySide6 import QtWidgets
+from user_interface import UserInterface
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = UserInterface()
+window.show()
+
+app.exec()
